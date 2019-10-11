5fef395ef586950bc8084680ab567ed757293c19
gracexinran <gracexinran@gmail.com>
2019-10-11 00:47:02 -0700

    completed

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,23 @@
 
 def palindrome_permutation?(string)
+  hash = {}
+  leng = string.length
+
+  i = 0
+  while i < leng
+    if hash[string[i]]
+      hash[string[i]] += 1
+    else
+      hash[string[i]] = 1
+    end
+    i += 1
+  end
+
+  odd_v = 0
+  hash.each do |k, v|
+    odd_v += 1 if v.odd?
+  end
+
+  return odd_v <= 1
   
 end
