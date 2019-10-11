1f522d045dc071a779ea1d16701a2945c9ab4826
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-10 21:56:44 -0700

    Created intersection method

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,16 @@
 def intersection(list1, list2)
-  
+  lookup_hash = {}
+  results = []
+
+  list1.each do |num|
+    lookup_hash[num] = num
+  end
+
+  list2.each do |num|
+    if lookup_hash.has_value?(num)
+      results << num
+    end
+  end
+
+  return results
 end
