0ead9c68c88622cb0d2aeda277fa68d935fe4eef
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-11 20:29:31 -0700

    tested 100%

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,26 @@
 
 def palindrome_permutation?(string)
+  #takes a string as an argument and returns true if the letters could be re-arranged into a palindrome  
+  hash = {}
   
+  string.split("").each do |char|
+    if hash[char]
+      hash[char] += 1
+    else
+      hash[char] = 1
+    end
+  end
+  
+  found_an_odd_value = nil
+  hash.values.each do |value|
+    if value % 2 == 1
+      if found_an_odd_value
+        return false
+      else
+        found_an_odd_value = true
+      end
+    end
+  end
+  
+  return true
 end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,31 @@
 
 def permutations?(string1, string2)
+  # take two strings as arguments and returns true if one string is a permutation of the other
   
+  hash = {}
+  string1.split("").each do |char|
+    if hash[char]
+      hash[char] += 1
+    else
+      hash[char] = 1
+    end
+  end
+  
+  puts hash
+  string2.split("").each do |char|
+    if ! hash[char]
+      return false
+    elsif hash[char] == 1
+      hash[char] = nil
+    elsif hash[char] > 1
+      hash[char] -= 1
+    end
+  end
+  
+  hash.values.each do |value|
+    return false if value
+  end
+  
+  return true
 end
+
