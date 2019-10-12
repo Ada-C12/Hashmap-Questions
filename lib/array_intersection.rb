c600fb86384bb664658ac16728e66f552b5c0ca8
Mira <calopter@gmail.com>
2019-10-12 16:05:14 -0700

    pass

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,5 @@
 def intersection(list1, list2)
-  
+  h = {}
+  list1.each { |x| h[x] = true }
+  list2.select(&h.to_proc)
 end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,8 @@
-
 def permutations?(string1, string2)
+  h = Hash.new { |h, k| h[k] = 0 }
   
+  string1.each_char { |c| h[c] += 1 }
+  string2.each_char { |c| h[c] -= 1 }
+  
+  h.values.all?(&:zero?)
 end
