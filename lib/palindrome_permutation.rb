77bc6c236cae0be5426a2047343d5c7c144941ff
Dianna Tingg <dtingg@gmail.com>
2019-10-13 08:59:18 -0700

    finished palindrom permutations

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,20 @@
-
 def palindrome_permutation?(string)
+  letter_hash = {}
   
+  string.each_char do |char|
+    if letter_hash[char]
+      letter_hash[char] += 1
+    else
+      letter_hash[char] = 1
+    end
+  end
+  
+  odd_letters = 0
+  
+  letter_hash.each do |letter, number|
+    odd_letters += 1 if number % 2 != 0
+  end
+  
+  return true if odd_letters <= 1
+  return false
 end
