0b3dfe01561f6c6ca0881f8646673a91c96a3595
Dianna Tingg <dtingg@gmail.com>
2019-10-13 08:38:42 -0700

    finished array_intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,14 @@
 def intersection(list1, list2)
+  num_hash = {}
+  intersect_nums = []
   
+  list1.each do |num|
+    num_hash[num] = true
+  end
+  
+  list2.each do |num|
+    intersect_nums << num if num_hash[num]
+  end
+  
+  return intersect_nums  
 end
