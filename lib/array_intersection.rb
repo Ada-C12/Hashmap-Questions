f11462c9cc6491537a3afb52b8bfb9a5189f7c89
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-10-13 08:09:04 -0700

    added methods

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,16 @@
 def intersection(list1, list2)
-  
+  hash = {}
+  results = []
+
+  list1.each do |num|
+    hash[num] = true
+  end
+
+  list2.each do |num|
+    if hash[num]
+      results << num
+    end
+  end
+
+  return results
 end
--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,14 @@
 
 def palindrome_permutation?(string)
-  
+  hash = {}
+  results = []
+  string.each_char do |letter|
+    hash[letter] ? hash[letter] += 1 : hash[letter] = 1
+  end
+
+  string.each_char do |letter|
+    results << letter if hash[letter].odd?
+  end
+
+  return results.length <= 1
 end
