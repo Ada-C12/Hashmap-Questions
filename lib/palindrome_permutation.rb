8b6890ee6a8def036d55a249c78f171ababd7188
Kristina <kristina.tanya@gmail.com>
2019-10-12 21:42:31 -0700

    palindrome_permutation method passing tests

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,28 @@
-
 def palindrome_permutation?(string)
-  
+  letter_tracker = add_letters_to_hash(string)
+  odd_occurrences = 0
+
+  letter_tracker.each do |key, value|
+    if letter_tracker[key] % 2 != 0
+      odd_occurrences += 1
+    end
+  end
+
+  if odd_occurrences <= 1
+    return true
+  else 
+    return false
+  end
+end
+
+def add_letters_to_hash(string)
+  letter_counts = {}
+  string.chars.each do |letter|
+    if letter_counts.key?(letter)
+      letter_counts[letter] += 1
+    else 
+      letter_counts[letter] = 1
+    end
+  end
+  return letter_counts
 end
