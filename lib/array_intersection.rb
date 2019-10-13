a0a27cfae0c290d650b488f041844fa3a33358b9
Janice Huang <janicehuang@gmail.com>
2019-10-12 20:28:59 -0700

    solve three puzzles

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,15 @@
 def intersection(list1, list2)
-  
+  answers = []
+  hash= {}
+  list1.each do |num1|
+    hash[num1] = "present"
+  end
+
+  list2.each do |num2|
+    if hash.has_key?(num2)
+      answers << num2
+    end
+  end
+
+  return answers
 end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,31 @@
 
 def permutations?(string1, string2)
-  
+  if string1.length != string2.length
+    return false
+  end
+  index = 0
+  letter_hash = {}
+  while index != string1.length
+    if letter_hash.has_key?(string1[index])
+      letter_hash[string1[index]] += 1
+    else
+      letter_hash[string1[index]] = 1
+    end
+    index += 1
+  end
+
+  index = 0
+  while index != string2.length
+    if letter_hash.has_key?(string2[index]) == false
+      return false
+    else
+      letter_hash[string2[index]] -= 1
+    end
+    index += 1
+  end
+
+  if letter_hash.values.sum == 0
+    return true
+  else return false
+  end
 end
