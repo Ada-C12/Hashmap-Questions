5b4c23c0ad1129914354469f6fc50f8a176dc287
Julia Bouvier <juliabouv@gmail.com>
2019-10-14 14:16:01 -0700

    Implemented permutations? method. Tests pass

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,21 @@
-
 def permutations?(string1, string2)
+  hash = {}
+  string1.each_char do |character|
+    unless hash.has_key?(character)
+      hash[character] = 1
+    else 
+      hash[character] += 1
+    end
+  end
+  p hash
   
+  string2.each_char do |letter|
+    if hash.has_key?(letter) && hash[letter] > 0
+      hash[letter] -= 1
+      p hash
+    else
+      return false
+    end
+  end
+  return hash.values.all?(0)
 end
