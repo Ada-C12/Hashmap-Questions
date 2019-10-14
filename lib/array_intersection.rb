a6819e148068f88991e36715c0a370aeb9024214
Julia Bouvier <juliabouv@gmail.com>
2019-10-14 13:21:23 -0700

    Implemented intersection method for array_intersection. Tests pass

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,15 @@
 def intersection(list1, list2)
-  
+  hash = {}
+  list1.each do |element|
+    hash[element] = true
+  end
+
+  result = []
+  list2.each do |number|
+    if hash[number]
+      result << number
+    end
+  end
+
+  return result
 end
