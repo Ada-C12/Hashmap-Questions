b2d3565a0a878dd4f232541984b50ee59ea41f0c
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 08:11:51 -0700

    implemented permutations

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -2,12 +2,47 @@
 # if each contain only the same letters in the same quantities as the other.
 def permutations?(string1, string2)
   # create a hash
-  # create an output list
+  hash = {}
+  # set output to true
+  output = true
   
   # for each string1 character,
-  # make it a key in the hash and set it to true
+  string1.each_char do |char1|
+    # make it a key in the hash
+    # and set its value to the number of times it appears
+    if hash.has_key?(char1)
+      hash[char1] += 1
+    else
+      hash[char1] = 1
+    end
+  end
   
   # for each string2 character,
-  # check if it's a key in the hash.
-  # if it is, 
+  string2.each_char do |char2|
+    # check if it's a key in the hash
+    # and that it has a value greater than none
+    if hash.has_key?(char2) && hash[char2] > 0
+      # if it is, remove one from that hash value
+      hash[char2] -= 1
+    else
+      # otherwise, make output false
+      # b/c there is a not-shared letter
+      output = false
+    end
+  end
+  
+  # for each string1 character
+  string1.each_char do |char3|
+    # if there is a value in the hash
+    # for this character
+    if hash.has_key?(char3) && hash[char3] > 0
+      # set output to false
+      # b/c this char occurred more
+      # in string1 than string2
+      output = false
+    end
+  end
+  
+  # return output boolean
+  return output
 end
