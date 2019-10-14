9e4499ea25834138a33b737ddf828db05f19bc7f
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 07:56:24 -0700

    implemented array intersection

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,13 @@
 # returns true if one string is a permutation of the other
+# if each contain only the same letters in the same quantities as the other.
 def permutations?(string1, string2)
+  # create a hash
+  # create an output list
   
+  # for each string1 character,
+  # make it a key in the hash and set it to true
+  
+  # for each string2 character,
+  # check if it's a key in the hash.
+  # if it is, 
 end
