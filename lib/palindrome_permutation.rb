ac66b84da7ecf5bf6669718dfe522fed25d99f72
Hallie <idhallie@gmail.com>
2019-10-14 19:49:17 -0700

    Refactored and ready to submit.

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -7,7 +7,6 @@
 def palindrome_permutation?(string)
   letters = {}
   length = string.length
-  letters_in_even_numbers = 0
   letters_in_odd_numbers = 0
   
   # Put string into a hash. Key = letter; value = count of letter
@@ -21,20 +20,17 @@ def palindrome_permutation?(string)
   
   # Check each letter in hash for whether it is represented an even or odd number of times
   letters.each do |key, value|
-    if value % 2 == 0
-      letters_in_even_numbers += 1
-    else
+    if value % 2 == 1
       letters_in_odd_numbers += 1
     end 
   end
   
-  # An string with an even length should only have letters represented in an even number. An odd length should have just one letter represented an odd number of times.
+  # A string with an even length should only have letters represented in an even number. An odd length should have just one letter represented an odd number of times.
   if length % 2 == 0 && letters_in_odd_numbers == 0
     return true
-  elsif length % 2 == 1 && letters_in_odd_numbers == 1
+  elsif letters_in_odd_numbers == 1
     return true
   else
     return false
   end
-  
 end
