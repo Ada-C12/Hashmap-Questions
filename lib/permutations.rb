4c75813a7b9226df12de4bb339447019211fa570
Monick <monick.keo@gmail.com>
2019-10-14 20:10:52 -0700

    completed all three problems

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -10,6 +10,7 @@ def permutations?(string1, string2)
       hash[letter] = 1
     end 
   end
+
   string2.each do |letter|
     if hash[letter] != nil
       hash[letter] -= 1
@@ -17,6 +18,7 @@ def permutations?(string1, string2)
       hash[letter] = -1
     end
   end
+  
   hash.each do |letter, count|
     if count != 0
       return false
