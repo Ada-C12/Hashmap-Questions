7c32db1eeb42eedc44e2ffc391268c4eecdcbd38
daniela sanchez <dnsanche@gmail.com>
2019-10-15 20:32:51 -0700

    Finished

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,19 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  hash = {}
+  intersection = []
+  
+  list1.each do |element|
+    hash.store(element, true)
+  end  
+
+  list2.each do |element|
+    if hash[element] != nil
+      intersection << element
+    end
+  end
+
+  return intersection
 end
 
+#Time complexity: O(n)
+#Space complexity: O(n)
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,22 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  hash = {}
+  string_permutation = true
+
+  if string1.length != string2.length
+    string_permutation = false
+  else
+      string1.each_char do |character|
+        hash.store(character, true)
+      end
+      
+      string2.each_char do |character|
+        if hash[character] == nil
+          string_permutation = false
+        end
+      end
+  end
+  return string_permutation
 end
+
+#Time complexity: O(n)
+#Space complexity: O(n)
