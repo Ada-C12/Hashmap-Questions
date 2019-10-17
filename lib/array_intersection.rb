c49a42275fffb04648ccaaa1bc72ba170eb17a43
Eve Le <nguyenthao.lee@gmail.com>
2019-10-17 15:29:04 -0700

    Implement array intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,12 @@
 def intersection(list1, list2)
-  
+  raise ArgumentError unless (list1.is_a?(Array) && list2.is_a?(Array))   
+  hash = {}
+  list1.each { |element| hash[element] = true }
+  result = []
+  list2.each do |element|
+    if hash[element] == true
+      result << element
+    end
+  end
+  return result
 end
