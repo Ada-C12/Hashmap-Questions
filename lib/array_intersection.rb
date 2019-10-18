39bceb561514a65a2044c6502194e9474ba007bb
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-18 15:22:40 -0700

    Wrote method for array_interection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,21 @@
 def intersection(list1, list2)
+
+  i = 0
+  # k = 0
+
+  length_1 = list1.length
+  # length_2 = list2.length
+  intersection = []
+
+  until i > length_1
+    list2.each do |num|
+      if list1[i] == num
+        intersection << list1[i]
+      end
+    end
+    i += 1
+  end
+
+  return intersection
   
 end
