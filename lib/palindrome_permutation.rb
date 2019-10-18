fa5aa4b45b08616f7eb98fa1cf3e6d67e0cd02f2
Eve Le <nguyenthao.lee@gmail.com>
2019-10-17 23:27:02 -0700

    Implement palindrome permutation

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,23 @@
 
 def palindrome_permutation?(string)
+  raise ArgumentError unless string.instance_of?(String)
   
+  letter_count_hash = {}
+  string.each_char do |letter|
+    if letter_count_hash[letter] == nil
+      letter_count_hash[letter] = 1
+    else
+      letter_count_hash[letter] += 1
+    end
+  end
+  
+  odd_count = 0
+  letter_count_hash.each_value do |value|
+    if (value % 2) != 0
+      odd_count += 1
+      return false if odd_count > 1
+    end
+  end
+  
+  return true
 end
