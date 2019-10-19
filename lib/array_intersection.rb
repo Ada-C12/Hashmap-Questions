b0c345a9266b5417b3b7a30845e76ec13c7c3e8f
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-10-18 22:49:21 -0700

    solutions to problems

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,16 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  hash ={}
+  solution_array = []
+  
+  list1.each do |num|
+    hash[num] = true
+  end
+  
+  list2.each do |num|
+    if hash[num]
+      solution_array << num
+    end
+  end
+  return solution_array
 end
 
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,20 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  hash = {}
+  string1.each do |letter|
+    if hash[letter]
+      hash[letter] += 1
+    else
+      hash[letter] = 1
+    end
+  end
+  
+  string2.each do |letter|
+    if hash[letter] && hash[letter] != 0
+      hash[letter] -= 1
+    else
+      return false
+    end
+    return true
+  end
 end
