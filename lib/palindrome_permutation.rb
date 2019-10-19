911b0d3b219247b8877ffb4f4cfae6048972d113
Farah Davoodi <farahdavoodi@gmail.com>
2019-10-19 15:48:02 -0700

    completed palindrome permutation and passed tests

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -2,6 +2,34 @@
 def palindrome_permutation?(string)
   # raise NotImplementedError, "palindrome_permutation? not implemented"
 
-  
+  if string == ""
+    return true
+  end
+
+  new_hash = {}
+  i = 0
+  while i < string.length
+    if new_hash[string[i]]
+      new_hash[string[i]] += 1
+    else
+      new_hash[string[i]] = 1
+    end
+    i += 1
+  end
+
+  odd_character = 0
+  new_hash.each do |char, value|
+    if value % 2 == 1
+      odd_character += 1
+    end
+  end
+
+  if odd_character > 1
+    return false
+  else
+    return true
+  end
+
+
 end
 
