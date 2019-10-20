0c15600380eed02cada7dc4f2f0f860a28cb0aaf
Alice <alicesunhi@gmail.com>
2019-10-20 16:50:27 -0700

    hashmap

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,25 @@
-
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  return true if string.length == 0 || string == string.reverse
+  
+  hash = {}
+  
+  string.each_char do |char|
+    if hash.key?(char)
+      hash[char] += 1
+    else 
+      hash[char] = 1
+    end
+  end
+  
+  odd = 0
+  
+  hash.each_value do |val|
+    if val%2 != 0
+      odd +=1
+    end
+    
+    return false if odd > 1
+  end
+  
+  return true
 end
-
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,29 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  return true if string1 == string2
+  
+  hash = {}
+  
+  string1.each_char do |char|
+    if hash.key?(char)
+      hash[char] += 1
+    else 
+      hash[char] = 1
+    end
+  end
+  
+  string2.each_char do |char|
+    if hash.has_key?(char) && hash[char] > 0
+      hash[char] -= 1
+    else
+      return false
+    end
+  end
+  
+  hash.each_value do |val|
+    if val != 0
+      return false
+    end
+  end
+  
+  return true
 end
