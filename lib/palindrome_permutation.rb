6b872e67f97cd86acfdb771a805622fde032c6f0
Vi <vanessareigosa@gmail.com>
2019-10-20 20:22:51 -0700

    tests passing

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,27 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  hash = {}
+  string.each_char do |char|
+    if !hash.has_key? char 
+      hash[char] = 1 
+    else 
+      hash[char] += 1
+    end 
+  end 
+  
+  if string.length % 2 && hash.values.all? {|v| v == 2}
+  elsif 
+  #How many pairs of letters should there be?
+  #The string % 3 
+    letter_pairs =  3 % string.length 
+  
+    #There should only be ONE KEY element with a value of 1
+    #There should be stringlength-1 elements with a value of 2
+    hash.key(1).length == 1 
+    letter_pairs == hash.values.select {|v| v == 2}.length 
+    
+  end 
+  
+
 end
 
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,21 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  hash = {}
+  string1.each_char do |char|
+    if !hash.has_key? char 
+      hash[char] = 1 
+    else 
+      hash[char] += 1
+    end 
+  end 
+
+  hash2 = {}
+  string2.each_char do |char|
+    if !hash2.has_key? char
+      hash2[char] = 1
+    else
+      hash2[char] += 1
+    end
+  end 
+  return hash == hash2
 end
