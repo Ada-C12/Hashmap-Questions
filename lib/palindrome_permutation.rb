87acca90706b76142597c1fed1b87eebde5c5f3d
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-21 16:16:32 -0700

    Finished palidrome. Passes all tests.

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,30 @@
-
+require "pry"
 def palindrome_permutation?(string)
-  
+  # binding.pry
+  if string == ""
+    return true
+  end
+  string_array = string.chars
+  hash = Hash.new()
+
+  string_array.each do |char|
+    if hash.include? char
+      hash[char] += 1
+    else
+      hash[char] = 1 
+    end
+  end
+
+  odd = 0
+  hash.each_value do |value|
+    if value % 2 != 0
+      odd += 1
+    end
+  end
+  if odd > 1 
+    return false
+  else
+    return true
+end
+
 end
