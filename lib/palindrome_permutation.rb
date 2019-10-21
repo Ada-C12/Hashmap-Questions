5efa3a88cbc55ec10f9c46c3e4fa3ae37c361dc4
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-20 17:31:58 -0700

    done

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,23 @@
+#space complexity: O(n), where n is length of string (space for hash)
+#time complexity: O(n), where n is length of string (time to build the hash) 
 
 def palindrome_permutation?(string)
+  string_hash = {}
+  #add all letters of string to a hash: {letter => occurrences}
+  string.each_char do |char|
+    if string_hash[char]
+      string_hash[char] += 1 
+    else 
+      string_hash[char] = 1
+    end 
+  end 
   
-end
+  #return false if > 1 letter has an odd number of occurrences
+  odd_letter_counter = 0
+  string_hash.each_value do |value| 
+    odd_letter_counter += 1 if value % 2 != 0
+    return false if odd_letter_counter > 1
+  end
+  return true  
+end 
+
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,30 @@
+#space complexity: O(n), where n is the number of elements in string1
+#time complexity: O(n), where n is the number of elements in string1 (time required to build the hash)
 
 def permutations?(string1, string2)
+  return false if string1.length != string2.length
+  string1_hash = {}
   
-end
+  #Add each char in string1 to a hash, {char => # of occurrences}
+  string1.each_char do |char|
+    if string1_hash[char]
+      string1_hash[char] += 1 
+    else 
+      string1_hash[char] = 1
+    end 
+  end 
+  
+  #Check whether each char of string2 exists in string1_hash, decrement hash values accordingly.
+  string2.each_char do |char|
+    if string1_hash[char].nil?
+      return false
+    elsif string1_hash[char] > 1
+      string1_hash[char] -= 1 
+    else 
+      string1_hash.delete(char)
+    end 
+  end 
+  
+  #if string1_hash is empty, the strings are palindromes
+  return string1_hash.empty? ? true : false 
+end 
