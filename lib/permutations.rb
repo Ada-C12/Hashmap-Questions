cc7815cef9e9c1e3617b10091dea4f10f8724ad4
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-21 00:21:15 -0700

    array_intersection and permutations

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,22 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  characters = {}
+  i = 0
+  j = 0
+  same_char = 0
+
+  if string1.length == string2.length
+    while i < string1.length
+      characters[string1[i]] = 1
+      i += 1
+    end
+
+    while j < string1.length
+      if characters[string2[j]] == 1
+        same_char += 1
+      end
+      j += 1
+    end
+    return same_char == string1.length
+  end
+  return false
 end
