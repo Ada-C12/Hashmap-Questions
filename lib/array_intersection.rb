5040ae49de66965b04ebf97d9d8082d80e1f5b8f
Yasmin <yasmin312002@hotmail.com>
2019-10-21 20:26:28 -0700

    all tests are passed

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,13 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  hash = {}
+  array = []
+  list1.each do |num|
+    hash[num] = 0
+  end
+  list2.each do |num|
+    if hash.key?(num)
+      array << num
+    end
+  end
+  return array
 end
-
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,27 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  hash = {}
+  string1 = string1.split("")
+  string2 = string2.split("")
+
+  string1.each do |char|
+    if hash[char]
+      hash[char] += 1
+    else
+      hash[char] = 1
+    end
+  end
+  string2.each do |char|
+    if hash[char]
+      hash[char] -= 1
+    else
+      return false
+    end
+  end
+  hash.each do |char, value|
+    if value != 0
+      return false
+    end
+  end
+  return true
 end
