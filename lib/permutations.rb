810a9e2f9feb7ca443fd42287d1da532a95d6778
brilatimer <brihiramine@gmail.com>
2019-10-21 20:29:24 -0700

    problem 1 and 2

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,19 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  hash = Hash.new(0)
+  if string1.length != string2.length 
+    return false
+  end
+  string1.split('').each do |letter|
+    hash[letter] += 1
+  end
+  
+  
+  string2.split('').each do |letter|
+    if hash.include?(letter) && hash[letter] > 0 
+      hash[letter] -= 1
+    else
+      return false
+    end
+  end
+  return true
 end
