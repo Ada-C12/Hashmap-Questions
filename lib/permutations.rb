dadb71d47a4bfea2b21878a80d403baee0d95f31
Erika <erika.maust@gmail.com>
2019-10-21 19:09:34 -0700

    Test value expect was false, set to true (like description said). Permutation method created and passes test.

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,28 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  
+  if string1.empty? || string2.empty?
+    return true
+  elsif string1.length != string2.length
+    return false
+  end
+
+
+  i = string1.chars
+  j = string2.chars
+  hash = {}
+  hash1 = {}
+
+  
+  i.each do |char|
+    hash1[char] = true
+  end
+
+  j.each do |char|
+    if hash[char] == false
+      return false
+    else
+      return true
+    end
+  end
 end
