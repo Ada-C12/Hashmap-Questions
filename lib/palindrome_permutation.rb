87a621214458b3e63687d5232863878b4e0dcd65
Morgan Schuler <morganlschuler@gmail.com>
2019-10-21 17:50:52 -0700

    Finished problem 1

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,3 +1,5 @@
+# Write a method which will take two strings as arguments and returns true if one string is a permutation of the other.
+
 
 def palindrome_permutation?(string)
   raise NotImplementedError, "palindrome_permutation? not implemented"
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,3 +1,5 @@
+# Write a method which takes a string as an argument and returns true if the letters could be re-arranged into a palindrome.
+
 
 def permutations?(string1, string2)
   raise NotImplementedError, "permutations? not implemented"
