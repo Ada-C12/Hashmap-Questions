e7e65a787955261c24c0f0126e913fab79910619
Mariya Burrows <mariya.burrows@gmail.com>
2019-10-21 20:30:20 -0700

    completed hash table questions

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,19 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  hash = {}
+  intersect = []
+  
+  list1.each do |num|
+    if hash[num].nil?
+      hash[num] = 1
+    end
+  end
+  
+  list2.each do |num|
+    if hash[num]
+      intersect << num
+    end
+  end
+  
+  return intersect
 end
 
