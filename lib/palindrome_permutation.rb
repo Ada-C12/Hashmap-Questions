6986f25b215fcc31b1927c1bb116aab3b6fd2e5f
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-22 22:29:39 -0700

    palindrome permutation method

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,26 @@
 def palindrome_permutation?(string)
+  hash = {}
+  i = 1
+  nones = 0
+  
+  string.each_char do |char|
+    if hash[char] == nil
+      hash[char] = i
+    else
+      hash[char] += 1
+    end
+  end
 
+  hash.each do |char, times|
+    if times % 2 != 0
+      nones += 1
+    end
+  end
+
+  if nones > 1
+    return false
+  else
+    return true
+  end
 end
 
