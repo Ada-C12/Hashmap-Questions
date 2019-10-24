dadd9d2933cbe054345890b1d6b20921820fa04b
Dominique <dom_taylor@icloud.com>
2019-10-23 20:23:34 -0700

    submission

--- test/palindrome_permutation_test.rb
+++ test/palindrome_permutation_test.rb
@@ -1,6 +1,6 @@
 require_relative "test_helper"
 
-xdescribe "palindrome_permutation?" do
+describe "palindrome_permutation?" do
   it "will work for hello" do
     expect(palindrome_permutation?("hello")).must_equal false
   end
@@ -11,5 +11,16 @@ xdescribe "palindrome_permutation?" do
 
   it "will work for emptystring" do
     expect(palindrome_permutation?("")).must_equal true
+  end
+
+  it "test1" do
+    expect(palindrome_permutation?("eeaa")).must_equal true
+    expect(palindrome_permutation?("eeeb")).must_equal false
+    expect(palindrome_permutation?("eebc")).must_equal false
+    expect(palindrome_permutation?("eeb")).must_equal true
+    expect(palindrome_permutation?("aaaa")).must_equal true
+    expect(palindrome_permutation?("1234")).must_equal false
+    expect(palindrome_permutation?("aaaee")).must_equal true
+
   end
 end
