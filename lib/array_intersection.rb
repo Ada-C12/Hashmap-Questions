603f0b7fbe55bdf0f3efe1015b89f514b93034dd
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-10 14:13:45 -0800

    Passing all tests for array_intersection.

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,25 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  list1_hash = {}
+  intersection = []
+  
+  # create the hashmap for list1
+  list1.each do |number|
+    if list1_hash.include? number
+      list1_hash[number] += 1
+    else 
+      list1_hash[number] = 1
+    end
+  end
+  
+  # compare list2 to the hash -- if there's a match, put the number in the intersection array
+  
+  list2.each do |number|
+    if list1_hash.include? number
+      intersection << number
+      # decrement the tally for the number in the hash so duplicates are handled appropriately
+      list1_hash[number] -= 1
+    end
+  end
+  return intersection
 end
 
