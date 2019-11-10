89db512ddca1635c2abcb0c5a6031fe76425e31b
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-10 15:11:09 -0800

    Passing all palindrome tests even though I haven't added logic for situations where letter counts aren't even. Why?

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,40 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  
+  # palindrome rules: 
+  #  - if count of all letters is even, count of each letter must also be even
+  #  - if count of all letters is odd, count of all but one letter must be even.
+  
+  
+  # account for an empty string
+  if string == "" 
+    return true
+  end
+  
+  # make a hash map
+  hash = {}
+  string.chars.each do |character|
+    if hash[character] == nil
+      hash[character] = 1
+    else
+      hash[character] += 1
+    end
+  end
+  
+  # apply logic for palindrome rules
+  if hash.keys.count % 2 == 0
+    hash.values.each do |value|
+      if value % 2 == 0
+        return true
+      else
+        return false
+      end
+    end 
+    # elsif
+    #   if hash.keys.count % 2 == 1
+    #   end
+  else
+    return false
+  end
+  
 end
-
--- test/palindrome_permutation_test.rb
+++ test/palindrome_permutation_test.rb
@@ -1,15 +1,23 @@
 require_relative "test_helper"
 
-xdescribe "palindrome_permutation?" do
+describe "palindrome_permutation?" do
+  # letters with odd and even counts
   it "will work for hello" do
     expect(palindrome_permutation?("hello")).must_equal false
   end
-
+  
+  # one letter with an odd count
   it "will work for 'carrace'" do
     expect(palindrome_permutation?("carrace")).must_equal true
   end
-
+  
   it "will work for emptystring" do
     expect(palindrome_permutation?("")).must_equal true
   end
+  
+  # count of all letters is odd
+  it "returns false for 'rhythm'" do
+    expect(palindrome_permutation?("phooey")).must_equal false
+  end
+  
 end
